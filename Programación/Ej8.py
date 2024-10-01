f55e2d15fafd61427cc7729f27f8d4ffbb180868
vic942tor <vovvictor@gmail.com>
2024-10-01 12:13:55 +0100

    Update Ej8.py

--- Programación/Ej8.py
+++ Programación/Ej8.py
@@ -1,4 +1,9 @@
 #Crear un modo de lectura de nombre y apellidos separados por una ,
-ap = input("Introduzca su nombre")
-ad = input("Introduzca sus apellidos")
-print(f"{ad},{ap}")
+# nombre = input("Introduzca su nombre: ")
+# apellido = input("Introduzca sus apellidos: ")
+
+# print(f"{apellido},{nombre}")
+#Leer nombre y apellidos conjuntos
+Cant2 = input("Cuantos apellidos tienes (establecelo en número): ")
+cant = input("Cuantos nombres tienes (establecelo en número): ")
+nom = input("Intoduce nombre y apellidos: ")
