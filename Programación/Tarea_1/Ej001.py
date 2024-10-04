fe5139c081132e6a4a3f2f0aec286bc9f836acb5
vic942tor <vovvictor@gmail.com>
2024-10-04 08:24:37 +0100

    Update Ej001.py

--- Programación/Tarea_1/Ej001.py
+++ Programación/Tarea_1/Ej001.py
@@ -2,8 +2,10 @@
 # será valido si es mayor o igual a 2 y menor o igual a10 o menor o igual a -2 y
 # mayor o igual que -10. El programa deberá imprimir según el número leído el
 # patrón de figura que se muestra en el cuadro de abajo
-valor = input("Establece un valor entre 2/10 y -2/-10: ")
-if valor >= "2" and valor <= "10":
+valor = int(input("Establece un valor entre 2/10 y -2/-10: "))
+if valor >= 2 and valor <= 10:
      print("adios mundo")
-if valor <= "-2" and valor >= "-10":
+elif valor <= -2 and valor >= -10:
     print("hola mundo")
+else:
+     print("Adios mundo")
