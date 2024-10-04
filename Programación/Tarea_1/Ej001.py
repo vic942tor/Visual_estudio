fb30e41a2b4e755efcc3e889d7fbbf258cd85a4f
vic942tor <vovvictor@gmail.com>
2024-10-04 08:55:06 +0100

    Update Ej001.py

--- Programación/Tarea_1/Ej001.py
+++ Programación/Tarea_1/Ej001.py
@@ -2,46 +2,14 @@
 # será valido si es mayor o igual a 2 y menor o igual a10 o menor o igual a -2 y
 # mayor o igual que -10. El programa deberá imprimir según el número leído el
 # patrón de figura que se muestra en el cuadro de abajo
-valor = int(input("Establece un valor entre 2/10 y -2/-10: "))
-if valor >= 2 and valor <= 10:
-    if valor == 2:
-          pass
-    elif valor == 3:
-          pass
-    elif valor == 4:
-          pass
-    elif valor == 5:
-          pass
-    elif valor == 6:
-          pass
-    elif valor == 7:
-          pass
-    elif valor == 8:
-          pass
-    elif valor == 9:
-          pass
-    elif valor == 10:
-          pass
+import time
+while True:
+    valor = int(input("Establece un valor entre 2/10 y -2/-10: "))
+    if valor >= 2 and valor <= 10:
+        pass
+    if valor <= -2 and valor >= -10:
+        pass
     else:
-          pass
-elif valor <= -2 and valor >= -10:
-    if valor == -2:
-          pass
-    elif valor == -3:
-          pass
-    elif valor == -4:
-          pass
-    elif valor == -5:
-          pass
-    elif valor == -6:
-          pass
-    elif valor == -7:
-          pass
-    elif valor == -8:
-          pass
-    elif valor == -9:
-          pass
-    elif valor == -10:
-          pass
-    else:
-     print("Adios mundo")
+        print("valor no valido, por favor introduzca un valor valido")
+        time.sleep(2)
+        break
