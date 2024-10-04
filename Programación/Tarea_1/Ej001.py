c1946428a8315712a79d396404295efecc4d64de
vic942tor <vovvictor@gmail.com>
2024-10-04 08:30:25 +0100

    as

--- Programación/Tarea_1/Ej001.py
+++ Programación/Tarea_1/Ej001.py
@@ -4,8 +4,44 @@
 # patrón de figura que se muestra en el cuadro de abajo
 valor = int(input("Establece un valor entre 2/10 y -2/-10: "))
 if valor >= 2 and valor <= 10:
-     print("adios mundo")
+    if valor == 2:
+          pass
+    elif valor == 3:
+          pass
+    elif valor == 4:
+          pass
+    elif valor == 5:
+          pass
+    elif valor == 6:
+          pass
+    elif valor == 7:
+          pass
+    elif valor == 8:
+          pass
+    elif valor == 9:
+          pass
+    elif valor == 10:
+          pass
+    else:
+          pass
 elif valor <= -2 and valor >= -10:
-    print("hola mundo")
-else:
+    if valor == -2:
+          pass
+    elif valor == -3:
+          pass
+    elif valor == -4:
+          pass
+    elif valor == -5:
+          pass
+    elif valor == -6:
+          pass
+    elif valor == -7:
+          pass
+    elif valor == -8:
+          pass
+    elif valor == -9:
+          pass
+    elif valor == -10:
+          pass
+    else:
      print("Adios mundo")
