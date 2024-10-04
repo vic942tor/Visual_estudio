1aecaad1d08f5770302f1e3f9b69819ad4c4ebb2
vic942tor <vovvictor@gmail.com>
2024-10-04 08:22:11 +0100

    Update Ej001.py

---
+++ Programación/Tarea_1/Ej001.py
@@ -0,0 +1,9 @@
+#Escriba un programa en Python, que pida al usuario un número entero. El número
+# será valido si es mayor o igual a 2 y menor o igual a10 o menor o igual a -2 y
+# mayor o igual que -10. El programa deberá imprimir según el número leído el
+# patrón de figura que se muestra en el cuadro de abajo
+valor = input("Establece un valor entre 2/10 y -2/-10: ")
+if valor >= "2" and valor <= "10":
+     print("adios mundo")
+if valor <= "-2" and valor >= "-10":
+    print("hola mundo")
