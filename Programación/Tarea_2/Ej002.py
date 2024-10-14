0d637e4a4ad1422b94a36d3919ac8b6d347bc4ae
vic942tor <vovvictor@gmail.com>
2024-10-14 09:23:42 +0100

    Update Ej002.py

---
+++ Programación/Tarea_2/Ej002.py
@@ -0,0 +1,5 @@
+# Un número de n dígitos es un número de Armstrong si es igual a la suma de las nésimas potencias de sus dígitos. 
+# Por ejemplo, 371, 8208 y 4210818 son números de Armstrong.
+# Escriba un programa en Python que muestre los 20 primeros números de
+# Armstrong.
+
