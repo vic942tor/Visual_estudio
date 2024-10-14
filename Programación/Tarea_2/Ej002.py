983db3ccd248951106541671519512ac2401dc16
vic942tor <vovvictor@gmail.com>
2024-10-14 09:40:28 +0100

    Update Ej002.py

--- Programación/Tarea_2/Ej002.py
+++ Programación/Tarea_2/Ej002.py
@@ -3,3 +3,13 @@
 # Escriba un programa en Python que muestre los 20 primeros números de
 # Armstrong.
 
+numeros_armstrong = []
+num = 0
+while len(numeros_armstrong) < 20:
+    digitos = str(num)
+    n = len(digitos)
+    suma_potencias = sum(int(digito) ** n for digito in digitos)
+    if suma_potencias == num:
+        numeros_armstrong.append(num)
+    num += 1
+print("Los primeros 20 números de Armstrong son los siguientes:", numeros_armstrong)
