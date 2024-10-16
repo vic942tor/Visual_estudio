8911fd46e3baee57a0710ec6f00740ab5243cb57
vic942tor <vovvictor@gmail.com>
2024-10-16 13:40:27 +0100

    56456

--- Programación/Ej16.py
+++ Programación/Ej16.py
@@ -1,12 +1,12 @@
 #Programa que muestre los 20 primeros múltiplos de un número ingresado por el usuario
 
 numero = input("Ingrese un número: ").capitalize()
-longitud = len(str(numero))
+longitud = len(numero)
 if not numero.isdigit():
     print("Debe ingresar un número entero.")
 else:
     for i in range(1 , 21):
         if  i % 5 != 0:
-            print(f"{int(numero) * i:{int(longitud + 1)}d}", end=" ")
+            print(f"{int(numero) * i:{int(longitud + 4)}d}", end=" ")
         else:
-            print(f"{int(numero) * i:{int(longitud + 1)}d}")
+            print(f"{int(numero) * i:{int(longitud + 4)}d}")
