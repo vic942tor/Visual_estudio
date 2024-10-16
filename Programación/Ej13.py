26e3f70a8bd2f5ea9cdf220e6efd886d6a2f95d9
vic942tor <vovvictor@gmail.com>
2024-10-16 12:41:13 +0100

    Update Ej13.py

--- Programación/Ej13.py
+++ Programación/Ej13.py
@@ -1,6 +1,8 @@
 #se lee un número por teclado y se imprime por pantalla todos los divisores que tiene 
 
 numero = int(input("Introduce un número: "))
-for i in range(1, numero +1):
+# for i in range(1, numero +1):
+for i in range(1, numero //2):
     if numero % i == 0:
         print(i, end =" ")
+print()
