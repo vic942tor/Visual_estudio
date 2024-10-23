d18bee9505ac8471471fa367514d7fe11c8a6db2
vic942tor <vovvictor@gmail.com>
2024-10-23 13:50:19 +0100

    Create Ej19.py

---
+++ Programación/Ejemplos/Ej19.py
@@ -0,0 +1,9 @@
+#Ordenar la lista de temperaturas en orden ascendente según la temperatura
+temp = [['Tfe',27.3,14.5],
+            ['Lpa',26.2,16.3],
+                ['Gom',20.3,17.3],
+                    ['hie',25.2,13.7]]
+for datos in temp:
+    print(type[datos[1]])
+
+
