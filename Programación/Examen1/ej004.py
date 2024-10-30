b9dce79ba68448cd9fbea504da096afd3f6cb7ab
vic942tor <vovvictor@gmail.com>
2024-10-30 10:22:48 +0000

    Update ej004.py

--- Programación/Examen1/ej004.py
+++ Programación/Examen1/ej004.py
@@ -9,12 +9,12 @@ ALFABETO = 'abcdefghijklmnñopqrstuvwxyz'
 ACENTOS = 'áéíóú'
 #Aquí estamos diciendole al programa la equivalencia de los acentos a las vocales normales de alfabeto
 frase = input('escriba una frase para saber si tiene todas las letras del alfabeto español: ').lower()
+frase = frase.replace('á','a')
+frase = frase.replace('é','e')
+frase = frase.replace('í','i')
+frase = frase.replace('ó','o')
+frase = frase.replace('ú','u')
 for i in ALFABETO:
-    ACENTOS[0] == ALFABETO[0]
-    ACENTOS[1] == ALFABETO[4]
-    ACENTOS[2] == ALFABETO[8]
-    ACENTOS[3] == ALFABETO[15]
-    ACENTOS[4] == ALFABETO[21]
     if i not in frase:
         print('No contiene todas las letras del alfabeto')
         break
