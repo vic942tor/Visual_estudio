386148bc601fe9e7158ae601fc668355328e5758
vic942tor <vovvictor@gmail.com>
2024-10-30 12:48:22 +0000

    hmj

--- Programación/Ejemplos/Ej20.py
+++ Programación/Ejemplos/Ej20.py
@@ -1,12 +1,18 @@
 #Realizar un programa que dado dos listas verifique que las listas estén solapadas o no
 
 A = [1,2,3,4]
-C = [2,0,9]
+C = [2,0,9,3,4]
+solapamiento = []
 
 for valor in A:
-    if valor in C:
-        print(f"Las listas están solapadas por {valor}")
-        break
+    if valor in C and valor not in solapamiento:
+        solapamiento.append(valor)
+
+if len(solapamiento) > 0:
+    print ('Las listas están solapadas')
+    for valor in solapamiento:
+        print(f'{valor}' , end="  ")
 else:
-    print("Las listas no están solapadas")
+    print ('Las listas no están solapadas')
+
 
