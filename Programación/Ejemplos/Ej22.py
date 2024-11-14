c1863dbca8fe055b41d9e37a3140bd3926326e87
vic942tor <vovvictor@gmail.com>
2024-11-14 13:02:14 +0000

    zc

---
+++ Programación/Ejemplos/Ej22.py
@@ -0,0 +1,34 @@
+#Almacenar los datos de los alumenos de 1ro DAM
+#alumnos = [(nombre,[bae,pro,lnd,ets,ssf,djk,itk,jkl])]
+ASIG = ['BAE','PRO','LND','ETS','SSF','DJK','ITK','JKL']
+Alumnos = []
+while True:
+    nombre = input('Introduzca el nombre, @ para finalizar: ')
+    if nombre == '@':
+         break
+    elif nombre.isnumeric():
+        print('Nombre no válido')
+        continue
+        tupla = (nombre,[])
+        for asig in ASIG:
+                while True:
+                    valor = input(f'Nota de {asig}: ')
+                    if valor.isdigit():
+                        nota = int(valor)
+                        break
+                    elif valor.upper() == 'NO':
+                        nota = valor
+                        break
+                    else:
+                        print('Nota no valida')
+                tupla[1].append(nota)
+        Alumnos.append(tupla)
+print(Alumnos)
+
+                    
+
+
+
+
+
+
