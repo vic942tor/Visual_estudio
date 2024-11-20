c6682ca8347bb59bec0eda77558ce00ce2bb51f0
vic942tor <vovvictor@gmail.com>
2024-11-20 12:52:23 +0000

    Update Ej001.py

--- Programación/Practica2/Ej001.py
+++ Programación/Practica2/Ej001.py
@@ -3,7 +3,7 @@
 # Para ello, le han contratado para generar un programa en Python que cree esa
 # estructura y pueda acceder a los datos que en ella se almacene.
 
-CATEGORIAS = ('frío', 'verduras', 'carnes', 'panadería', 'bazar', 'despensa', 'vinos', 'lácteos')
+CATEGORIAS = ('frío', 'verduras', 'carnes', 'panadería', 'bazar', 'despensa', 'winos', 'lácteos')
 productos = {}
 print(f'Tipos de productos disponibles: {", ".join(CATEGORIAS)}')
 while True:
@@ -15,27 +15,48 @@ while True:
         continue
     else:
         productos[tipo_producto] = {}
+#Valida el código
         while True:
-            codigo_numero = input('Introduzca el número del código del producto, máximo 4 digitos: ')
-            if len(codigo_numero) > 4 or len(codigo_numero) < 1 or codigo_numero.isalpha():
+            codigo_numero = input('Introduzca el número del código del producto, debe tener 4 digitos: ')
+            if len(codigo_numero) > 4 or len(codigo_numero) < 4 or codigo_numero.isalpha():
                 print('Este código no es valido, porfavor vuelva a introducirlo')
                 continue
             break          
-    #Genera el código del producto (primera letra del tipo de producto + número de código)
+#Genera el código del producto (primera letra del tipo de producto + número de código)
     codigo = tipo_producto[0].upper() + codigo_numero
     if codigo in productos[tipo_producto]:
         print(f'El código {codigo} ya está en uso.')
-        #Si el código ya existe, se modifican cantidad y precio
+#Si el código ya existe, se modifican cantidad y precio
         cantidad = float(input(f'Introduzca la cantidad para el producto con código {codigo}: '))
         precio = float(input(f'Introduzca el precio para el producto con código {codigo}: '))
-        #Actualiza cantidad y precio en el diccionario
+#Actualiza cantidad y precio en el diccionario
         productos[tipo_producto][codigo][1] += cantidad
         productos[tipo_producto][codigo][2] = round(precio, 2)
     else:
         descripcion = input('Introduzca la descripción del producto: ')
-        cantidad = float(input('Introduzca la cantidad del producto: '))
-        precio = float(input('Introduzca el precio del producto: '))
-        #Almacena el producto en el diccionario bajo el tipo y código correspondientes
+        while True:
+            try:
+                cantidad = float(input('Introduzca la cantidad del producto: '))
+            except:
+                print('Por favor, debe establecer la cantidad del producto')
+                continue
+            if cantidad < 0:
+                print("La cantidad introducida no puede ser negativa.")
+                continue
+            break
+        while True:
+            try:
+                precio = float(input('Introduzca el precio del producto: '))
+            except:
+                print('Por favor, debe establecer el precio del producto')
+                continue
+            if precio == '':
+                print('Por favor, debe establecer el precio del producto')
+            if precio < 0:
+                print("El precio introducido no puede ser negativo.")
+                continue
+            break
+#Almacena el producto en el diccionario bajo el tipo y código correspondientes
         productos[tipo_producto][codigo] = [descripcion, round(cantidad, 2), round(precio, 2)]
 #Muestra los productos almacenados
 print("\nProductos almacenados:")
@@ -46,7 +67,7 @@ for tipo, productos_tipo in productos.items():
         print(f"Código: {codigo}")
         print(f"Descripción: {descripcion}")
         print(f"Cantidad: {cantidad}")
-        print(f"Precio: {precio}€")
+        print(f"Precio: {precio:.2f}€")
 
     
 
